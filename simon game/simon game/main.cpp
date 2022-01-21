36f034ff7534568d3b4153c49e0de49a812081b7
(Student) - Szymon Staneczek <C00275789@itcarlow.ie>
2022-01-21 10:47:42 +0000

    end of session

--- simon game/simon game/main.cpp
+++ simon game/simon game/main.cpp
@@ -1,8 +1,8 @@
 /// <summary>
 /// simple game loop for SFML[2.5.1]
 /// 
-/// @author Peter Lowe
-/// @date May 2019
+/// @author Szymon Staneczek
+/// @date January 2022
 /// </summary>
 
 #ifdef _DEBUG 
